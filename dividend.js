25dde5d816fce901660098c19d0d5d186d5fcd31
jonathanlam1987 <jonathanlam@icloud.com>
2021-03-07 21:31:52 -0700

    added photo

---
+++ dividend.js
@@ -0,0 +1,16 @@
+
+
+
+
+let drop = document.getElementById("menu");
+
+
+
+function dropDown() {
+
+    if (drop.style.display === "block") {
+      drop.style.display = "none";
+    } else {
+      drop.style.display = "block";
+    }
+  }
